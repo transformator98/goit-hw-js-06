3af1ccca6662557f73f99f968d05573cae66bc62
transformator98 <vanushka16@gmail.com>
2020-08-10 23:19:59 +0300

    done task-07

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 import users from './users.js';
 
-/**
+/**=============================================================
  * task -01
  * Получить массив имен всех пользователей (поле name).
  */
@@ -19,7 +19,7 @@ import users from './users.js';
 //   'Sheree Anthony',
 // ];
 
-/**
+/**=============================================================
  * task -02
  * Получить массив объектов пользователей по цвету глаз (поле eyeColor).
  */
@@ -31,7 +31,7 @@ import users from './users.js';
 // console.log(getUsersWithEyeColor(users, 'blue'));
 // // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
 
-/**
+/**=============================================================
  * task -03
  * Получить массив имен пользователей по полу(поле gender).
  */
@@ -44,7 +44,7 @@ import users from './users.js';
 // console.log(getUsersWithGender(users, 'male'));
 // // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
-/**
+/**=============================================================
  * task -04
  *
  *  Получить массив только неактивных пользователей(поле isActive).
@@ -59,7 +59,7 @@ import users from './users.js';
 
 // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
 
-/**
+/**=============================================================
  * task -05
  *  Получить пользоваля (не массив) по email (поле email, он уникальный).
  *
@@ -74,7 +74,7 @@ import users from './users.js';
 // console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
 // // {объект пользователя Elma Head}
 
-/**
+/**=============================================================
  * task -06
  * Получить массив пользователей попадающих в возрастную категорию
  * от min до max лет (поле age).
@@ -91,18 +91,18 @@ import users from './users.js';
 // console.log(getUsersWithAge(users, 30, 40));
 // // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 
-/**
+/**=============================================================
  * task -07
  * Получить общую сумму баланса (поле balance) всех пользователей.
  */
 
 // const calculateTotalBalance = users => {
-//     // твой код
+//   return users.reduce((totalBallance, user) => totalBallance + user.balance, 0);
 // };
 
 // console.log(calculateTotalBalance(users)); // 20916
 
-/**
+/**=============================================================
  * task -08
  * Массив имен всех пользователей у которых есть друг с указанным именем.
  *
@@ -115,7 +115,7 @@ import users from './users.js';
 // console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
 // console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
 
-/**
+/**=============================================================
  * task -09
  * Массив имен (поле name) людей, отсортированных
  * в зависимости от количества их друзей (поле friends)
@@ -128,7 +128,7 @@ import users from './users.js';
 // console.log(getNamesSortedByFriendsCount(users));
 // // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
 
-/**
+/**=============================================================
  * task -10
  * Получить массив всех умений всех пользователей(поле skills), при этом не должно
  * быть повторяющихся умений и они должны быть отсортированы в алфавитном порядке.
